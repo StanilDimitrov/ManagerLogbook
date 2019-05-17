5424552b87a5cff42c6efe397afe12ea60c10824
Stenli <stanil_dimitrov@mail.bg>
2019-05-17 13:03:10 +0300

    Project skeleton added.

---
+++ ManagerLogbook/ManagerLogbook.Data/ManagerLogbookContext.cs
@@ -0,0 +1,17 @@
+﻿using ManagerLogbook.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ManagerLogbook.Data
+{
+    public class ManagerLogbookContext : IdentityDbContext<ApplicationUser>
+    {
+        public ManagerLogbookContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
